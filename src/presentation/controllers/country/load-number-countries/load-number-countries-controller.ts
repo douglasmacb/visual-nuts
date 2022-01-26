a3e2624c8b932e66ca75054c6cfbc05978e7bc12
Douglas Brito <douglasmacb@Douglass-MBP.Home>
2022-01-26 18:06:05 +0000

    feat: add number of countries in the world endpoint

--- src/presentation/controllers/country/load-number-countries/load-number-countries-controller.ts
+++ src/presentation/controllers/country/load-number-countries/load-number-countries-controller.ts
@@ -1,12 +1,12 @@
 import { ok, serverError, NumberCountry, Controller, HttpRequest, HttpResponse, LoadNumberCountries } from './load-number-countries-protocols'
 
 export class LoadNumberCountriesController implements Controller {
-  constructor (private readonly loadNumberCountries: LoadNumberCountries) { }
+  constructor (private readonly loadNumberCountries: LoadNumberCountries) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const number: NumberCountry = await this.loadNumberCountries.load()
-      return ok({ number })
+      const countries: NumberCountry = await this.loadNumberCountries.load()
+      return ok({ countries })
     } catch (error) {
       return serverError(error)
     }
