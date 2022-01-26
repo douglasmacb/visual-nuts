d01cbdb9ab78695ac8fbdc99d35c70c30bee949f
Douglas Brito <douglasmacb@Douglass-MBP.Home>
2022-01-26 15:37:03 +0000

    feat: initial architecture with all dependencies

--- src/main/server.ts
+++ src/main/server.ts
@@ -2,7 +2,7 @@ import env from '../main/config/env'
 
 const app = async (): Promise<any> => (await import('./config/app')).default
 
-app().then((server: any) => {
+app().then(server => {
   server.listen(env.port, () => console.log(`Server is running on port ${env.port}`))
 }).catch(error => {
   console.error(error)
